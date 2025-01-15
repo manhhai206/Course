ac4b86625dbb4017207371565add933292124516
manhhai206 <manhtronghai2062002@gmail.com>
2025-01-15 17:17:04 +0700

    completed

--- Bai12_CAN/CAN_Receive/main.c
+++ Bai12_CAN/CAN_Receive/main.c
@@ -1,79 +1,122 @@
-#include "stm32f10x.h"
-#include "stm32f10x_rcc.h"
-#include "stm32f10x_gpio.h"
-#include "stm32f10x_can.h"
+#include "stm32f10x.h"                  // Device header
+#include "stm32f10x_rcc.h"              // Keil::Device:StdPeriph Drivers:RCC
+#include "stm32f10x_gpio.h"             // Keil::Device:StdPeriph Drivers:GPIO
+#include "stm32f10x_can.h"              // Keil::Device:StdPeriph Drivers:CAN
 
-uint8_t dataTransmit[4];
-
-void CAN_Clock_Configuration()
+/**
+ * @brief Configure the clocks for CAN and GPIO modules
+ */	
+ 
+void CAN_Clock_Configuration(void)
 {
-    RCC_APB1PeriphClockCmd(RCC_APB1Periph_CAN1, ENABLE);
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+	/* Enable clock for CAN1 on APB1 bus */
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_CAN1,ENABLE);
+	/* Enable clock for GPIOA on APB2 bus */
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
 }
+
+
+/**
+ * @brief Configure GPIOA pins for CAN functionaly.
+ */
 
 void CAN_GPIO_Configuration(void) 
 {
     GPIO_InitTypeDef GPIO_InitStructure;
-
+	
+    /* Configure PA11 (CAN_RX) as Input Pull-Up */
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
     GPIO_Init(GPIOA, &GPIO_InitStructure);
 
+    /* Configure PA12 (CAN_TX) as Alternate Function Push-Pull */
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_Init(GPIOA, &GPIO_InitStructure);
 }
 
-void CAN_Config()
+
+/** 
+ * @brief Configures the CAN module for transmission and reception.
+ */
+void CAN_Config(void)
 {
-    CAN_InitTypeDef CAN_InitStructure;
-
-    CAN_InitStructure.CAN_TTCM = DISABLE;
-    CAN_InitStructure.CAN_ABOM = ENABLE;
-    CAN_InitStructure.CAN_AWUM = ENABLE;
-    CAN_InitStructure.CAN_NART = DISABLE;
-    CAN_InitStructure.CAN_RFLM = DISABLE;
-    CAN_InitStructure.CAN_TXFP = DISABLE;
-    CAN_InitStructure.CAN_Mode = CAN_Mode_Normal;
-
-    CAN_InitStructure.CAN_SJW = CAN_SJW_1tq;
-    CAN_InitStructure.CAN_BS1 = CAN_BS1_6tq;
-    CAN_InitStructure.CAN_BS2 = CAN_BS2_8tq;
-    CAN_InitStructure.CAN_Prescaler = 6;
-
-    CAN_Init(CAN1, &CAN_InitStructure);
+	CAN_InitTypeDef CAN_InitStructure;
+	/* Initialize CAN configuration parameters */
+	CAN_InitStructure.CAN_TTCM = DISABLE; /**< Time Trigger Communication Mode */
+	CAN_InitStructure.CAN_ABOM = ENABLE; /**< Automatic Bus-Off Management */
+	CAN_InitStructure.CAN_AWUM = ENABLE; /**< Automatic Wake-up Mode */
+	CAN_InitStructure.CAN_NART = DISABLE; /**< No Automatic Retransmisstion */
+	CAN_InitStructure.CAN_RFLM = DISABLE; /**< Receive FIFO Locked Mode */
+	CAN_InitStructure.CAN_TXFP = DISABLE; /**< Transmit FIFO Priority */
+	CAN_InitStructure.CAN_Mode = CAN_Mode_Normal; /**< CAN mode : Normal */
+	
+	// Configure Bit timing
+	CAN_InitStructure.CAN_SJW = CAN_SJW_1tq; /**< Synchronization Jump Width = 1 time quanta */
+	CAN_InitStructure.CAN_BS1 = CAN_BS1_6tq; /**< Bit segment 1 = 6 time quanta */
+	CAN_InitStructure.CAN_BS2 = CAN_BS2_8tq; /**< Bit segment 2 = 8 time quanta */
+	CAN_InitStructure.CAN_Prescaler = 6;	/**< Baudrate 36MHz / Prescaler */
+	/***************************************
+	* Bit Rate = Can_Clock / (Prescaler * (Synchronization Segment + BS1 + BS2)) (bit per second)
+	*	Bit Time = (Prescaler * (Synchronization Segment + BS1 + BS2)) / CAN_Clock	(uS)
+	************************************* */
+	
+	/* Initialize CAN peripheral with the configuration */
+	CAN_Init(CAN1,&CAN_InitStructure);
 }
 
-void CAN_FilterConfiguration(void) {
+
+/** 
+ * @brief Configures the CAN filter for message reception.
+ */
+void CAN_FilterConfiguration(void) 
+{
     CAN_FilterInitTypeDef CAN_FilterInitStructure;
 
-    CAN_FilterInitStructure.CAN_FilterNumber = 0;
-    CAN_FilterInitStructure.CAN_FilterMode = CAN_FilterMode_IdMask;
-    CAN_FilterInitStructure.CAN_FilterScale = CAN_FilterScale_32bit;
-    CAN_FilterInitStructure.CAN_FilterIdHigh = 0x321 << 5;
+    /* Configure the CAN Filter parameters */
+    CAN_FilterInitStructure.CAN_FilterNumber = 0; /* Select Filter Bank 0 */
+    CAN_FilterInitStructure.CAN_FilterMode = CAN_FilterMode_IdMask; /**< Set filter mode to Mask Mode */
+    CAN_FilterInitStructure.CAN_FilterScale = CAN_FilterScale_32bit; /**< Use 32-bit filter */
+    CAN_FilterInitStructure.CAN_FilterIdHigh = 0x321 << 5; /**< Set Standard ID (Left-shift by 5) */
     CAN_FilterInitStructure.CAN_FilterIdLow = 0x0000;
-    CAN_FilterInitStructure.CAN_FilterMaskIdHigh = 0xFFE0;
+    CAN_FilterInitStructure.CAN_FilterMaskIdHigh = 0xFFE0; /**< Set mask to compare only the first 11 bits */
     CAN_FilterInitStructure.CAN_FilterMaskIdLow = 0x0000;
-    CAN_FilterInitStructure.CAN_FilterFIFOAssignment = CAN_FIFO0;
-    CAN_FilterInitStructure.CAN_FilterActivation = ENABLE;
+    CAN_FilterInitStructure.CAN_FilterFIFOAssignment = CAN_FIFO0; /**< Assign messages to FIFO0 */
+    CAN_FilterInitStructure.CAN_FilterActivation = ENABLE; /**< Enable the filter */
 
+    /* Initialize the CAN filter */
     CAN_FilterInit(&CAN_FilterInitStructure);
 }
 
-void CAN_ReceiveData(uint8_t* data)
-{
-    while(CAN_MessagePending(CAN1, CAN_FIFO0) < 1); 
-    CanRxMsg RxMessage;
-    
-    CAN_Receive(CAN1, CAN_FIFO0, &RxMessage);  
-    
-    for(int i = 0; i < RxMessage.DLC; i++) {
-        data[i] = RxMessage.Data[i];  
-    }
-    CAN_FIFORelease(CAN1, CAN_FIFO0);  
-}
 
+
+/**
+ * @brief Receives data from the CAN bus via FIFO0
+ * 
+ * @details This function waits for a CAN message in FIFO0, 
+ *          retrieves it, and stores its payload in the provided buffer.
+ */
+void CanIf_ReceiveData(uint8* DataPtr)
+{
+    while (CAN_MessagePending(CAN1, CAN_FIFO0) < 1) {
+        /* Ensures a message is available before proceeding */
+    }
+
+    /* Create a temporary structure to store the received CAN message */
+    CanRxMsg RxMessage;
+
+    /* Retrieve the message from FIFO0 */
+    CAN_Receive(CAN1, CAN_FIFO0, &RxMessage);
+
+    /* Copy the received payload into the provided buffer */
+    for (uint8 i = 0; i < RxMessage.DLC; i++) {
+        DataPtr[i] = RxMessage.Data[i];
+    }
+
+    /* Release the FIFO after reading the message */
+    CAN_FIFORelease(CAN1, CAN_FIFO0);
+}
 
 int main()
 {
--- Bai12_CAN/CAN_Transmit/main.c
+++ Bai12_CAN/CAN_Transmit/main.c
@@ -1,36 +1,49 @@
 #include "stm32f10x.h"                  // Device header
 #include "stm32f10x_rcc.h"              // Keil::Device:StdPeriph Drivers:RCC
 #include "stm32f10x_gpio.h"             // Keil::Device:StdPeriph Drivers:GPIO
-#include "stm32f10x_tim.h"              // Keil::Device:StdPeriph Drivers:TIM
 #include "stm32f10x_can.h"              // Keil::Device:StdPeriph Drivers:CAN
 
-void CAN_Clock_Configuration()
+/**
+ * @brief Configure the clocks for CAN and GPIO modules
+ */	
+ 
+void CAN_Clock_Configuration(void)
 {
-	/* Active Clock for CAN1 and GPIOA */
+	/* Enable clock for CAN1 on APB1 bus */
 	RCC_APB1PeriphClockCmd(RCC_APB1Periph_CAN1,ENABLE);
+	/* Enable clock for GPIOA on APB2 bus */
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
 }
 
-void CAN_GPIO_Configuration(void) 
-	{
-    GPIO_InitTypeDef GPIO_InitStructure;
 
-    // configure PA11 (CAN RX) is Input Pull-up
+/**
+ * @brief Configure GPIOA pins for CAN functionaly.
+ */
+
+void CAN_GPIO_Configuration(void) 
+{
+    GPIO_InitTypeDef GPIO_InitStructure;
+	
+    /* Configure PA11 (CAN_RX) as Input Pull-Up */
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
     GPIO_Init(GPIOA, &GPIO_InitStructure);
 
-    // configure PA12 (CAN TX) is Alternate Function Push-Pull
+    /* Configure PA12 (CAN_TX) as Alternate Function Push-Pull */
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_Init(GPIOA, &GPIO_InitStructure);
 }
-	
-void CAN_Config()
+
+
+/** 
+ * @brief Configures the CAN module for transmission and reception.
+ */
+void CAN_Config(void)
 {
 	CAN_InitTypeDef CAN_InitStructure;
-	
+	/* Initialize CAN configuration parameters */
 	CAN_InitStructure.CAN_TTCM = DISABLE; /**< Time Trigger Communication Mode */
 	CAN_InitStructure.CAN_ABOM = ENABLE; /**< Automatic Bus-Off Management */
 	CAN_InitStructure.CAN_AWUM = ENABLE; /**< Automatic Wake-up Mode */
@@ -48,39 +61,60 @@ void CAN_Config()
 	* Bit Rate = Can_Clock / (Prescaler * (Synchronization Segment + BS1 + BS2)) (bit per second)
 	*	Bit Time = (Prescaler * (Synchronization Segment + BS1 + BS2)) / CAN_Clock	(uS)
 	************************************* */
+	
+	/* Initialize CAN peripheral with the configuration */
 	CAN_Init(CAN1,&CAN_InitStructure);
 }
 
-void CAN_FilterConfiguration(void) {
+
+/** 
+ * @brief Configures the CAN filter for message reception.
+ */
+void CAN_FilterConfiguration(void) 
+{
     CAN_FilterInitTypeDef CAN_FilterInitStructure;
 
-    CAN_FilterInitStructure.CAN_FilterNumber = 0; /* Number of filter */
-    CAN_FilterInitStructure.CAN_FilterMode = CAN_FilterMode_IdMask;
-    CAN_FilterInitStructure.CAN_FilterScale = CAN_FilterScale_32bit;
-    CAN_FilterInitStructure.CAN_FilterIdHigh = 0x321 << 5;  // Standard ID 0x321
+    /* Configure the CAN Filter parameters */
+    CAN_FilterInitStructure.CAN_FilterNumber = 0; /* Select Filter Bank 0 */
+    CAN_FilterInitStructure.CAN_FilterMode = CAN_FilterMode_IdMask; /**< Set filter mode to Mask Mode */
+    CAN_FilterInitStructure.CAN_FilterScale = CAN_FilterScale_32bit; /**< Use 32-bit filter */
+    CAN_FilterInitStructure.CAN_FilterIdHigh = 0x321 << 5; /**< Set Standard ID (Left-shift by 5) */
     CAN_FilterInitStructure.CAN_FilterIdLow = 0x0000;
-    CAN_FilterInitStructure.CAN_FilterMaskIdHigh = 0xFFE0;  // Mask compare with 11-bit ID
+    CAN_FilterInitStructure.CAN_FilterMaskIdHigh = 0xFFE0; /**< Set mask to compare only the first 11 bits */
     CAN_FilterInitStructure.CAN_FilterMaskIdLow = 0x0000;
-    CAN_FilterInitStructure.CAN_FilterFIFOAssignment = CAN_FIFO0;  // Push in FIFO0
-    CAN_FilterInitStructure.CAN_FilterActivation = ENABLE;
+    CAN_FilterInitStructure.CAN_FilterFIFOAssignment = CAN_FIFO0; /**< Assign messages to FIFO0 */
+    CAN_FilterInitStructure.CAN_FilterActivation = ENABLE; /**< Enable the filter */
 
+    /* Initialize the CAN filter */
     CAN_FilterInit(&CAN_FilterInitStructure);
 }
 
-void CAN_TransmitData(uint8_t* data, uint8_t length) {
+
+/** 
+ * @brief Transmits data over the CAN bus.
+ * @param[in] data: Pointer to the data array to be transmitted.
+ * @param[in] length: Length of the data to be transmitted (max 8 bytes).
+ */
+void CAN_TransmitData(uint8_t* data, uint8_t length) 
+{
     CanTxMsg TxMessage;
 
-    TxMessage.StdId = 0x321;  
-    TxMessage.RTR = CAN_RTR_DATA;  
-    TxMessage.IDE = CAN_ID_STD;  
-    TxMessage.DLC = length; 
+    /* Configure the CAN message parameters */
+    TxMessage.StdId = 0x321; /**< Set Standard Identifier */
+    TxMessage.RTR = CAN_RTR_DATA; /**< Data Frame */
+    TxMessage.IDE = CAN_ID_STD; /**< Standard ID */
+    TxMessage.DLC = length; /**< Data Length Code */
 
-    for (int i = 0; i < length; i++) {
+    /* Copy the data to the CAN message */
+    for (int i = 0; i < length; i++) 
+    {
         TxMessage.Data[i] = data[i];
     }
 
+    /* Transmit the CAN message */
     uint8_t mailbox = CAN_Transmit(CAN1, &TxMessage); 
 
+    /* Wait until the message is successfully transmitted */
     while (CAN_TransmitStatus(CAN1, mailbox) != CAN_TxStatus_Ok);
 }
 
